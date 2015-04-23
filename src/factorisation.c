802bbc19af7f7169544c0c6ed46214a941c0e52c
Damien Deprez <damien.deprez@student.uclouvain.be>
2015-04-23 17:53:45 +0200

    ré-ajout de factorisation.c

---
+++ src/factorisation.c
@@ -0,0 +1,16 @@
+//
+// Created by damien on 22/04/15.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "factorisation.h"
+
+struct facteurPremier* factorisation (struct nombre* nombre1, struct facteurPremier* facteurPremier1)
+{
+	return NULL;
+}
+
+
+
