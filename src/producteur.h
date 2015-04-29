86d212bff9a4ea98f3b17a663434cf63f8ae8f72
Damien Deprez <damien.deprez@student.uclouvain.be>
2015-04-29 18:02:30 +0200

    Ajout des threads pour le producteur

--- src/producteur.h
+++ src/producteur.h
@@ -9,46 +9,44 @@
 #ifndef FACTORISATION_PRODUCTEUR_H
 #define FACTORISATION_PRODUCTEUR_H
 
+#include "buffer.h"
+
+struct thread_param
+{
+    struct buffer* buffer1;
+    char* inputName;
+    int fd_read;
+    int fd_write;
+};
+
 /**
- * readOneFromLocal
+ * readOneFromFD
  * lit un nombre depuis le descripteur fd
  *
  * @fd : descripteur correspondant à l'entreé d'où il faut lire les données
  * @inputName : nom de l'entrée d'origine du nombre
  * @return : structure nombre si erreur le nombre vaut 0
  */
-struct nombre readOneFromLocal (const int fd, const char* inputName);
+struct nombre readOneFromFD (const int fd, char* inputName);
 
 /**
- * readOneFromInternet
- * lit un nombre depuis un fichier se trouvant sur le résau
+ * produceFromFD
+ * lit depuis le descripteur de fichier et le place dans le buffer
  *
- * @URL : URL du fichier à lire
- * @return : structure nombre si erreur le nombre vaut 0
+ * @inputName: nom de l'entrée
+ * @fd: file descripteur utilisé pour la lecture
+ * @buffer1: pointeur vers le buffer
  */
-struct nombre readOneFromInternet (const char* URL);
 
-/**
- * produceFromFile
- * lit le fichier et place le contenu dans le buffer
- *
- * @filePath : chemin du fichier
- */
-void produceFromFile(const char* filePath);
-
-/**
- * produceFromStdin
- * lit l'entrée standard et place le contenu dans le buffer
- *
- */
-void produceFromStdin();
+void* produceFromFD(void * param);
 
 /**
  * produceFromInternet
- * lit le fichier distant et place le contenu dans le buffer
+ * lit le fichier distant et place le contenu dans un pipe
  *
  * @fileLocation : URL du fichier
+ * @fd: pipe où les données lue depuis le serveur seront écrite
  */
-void produceFromInternet(const char* fileLocation);
+void* produceFromInternet(void * param);
 
 #endif //FACTORISATION_PRODUCTEUR_H
