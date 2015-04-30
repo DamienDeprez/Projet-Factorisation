567264461b3aff57ad901eea291a9157bdad9c72
Damien Deprez <damien.deprez@student.uclouvain.be>
2015-04-30 16:54:10 +0200

    FIX  readFromFD for the case of the num of byte read is not enough

--- src/producteur.c
+++ src/producteur.c
@@ -8,6 +8,9 @@
 #include <string.h>
 #include <curl/curl.h>
 #include <inttypes.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <fcntl.h>
 
 #include "producteur.h"
 
@@ -22,8 +25,10 @@
 static size_t WriteMemoryPipe (void * ptr, size_t size, size_t nmem, void* data)
 {
 	int* fd  = (int*) data; // pointeur vers le pipe
-	return write(fd[1],ptr,size*nmem); // écris dans le pipe
-	//return size * nmem;
+	printf("pipe : fd %d\n",*fd);
+	ssize_t retour = write(*fd,ptr,size*nmem); // écris dans le pipe
+	printf("writen return : %lu\t donnée à lire %lu\n",retour,size*nmem);
+	return size*nmem;
 }
 
 struct nombre readOneFromFD (const int fd, char* inputName)
@@ -31,9 +36,10 @@ struct nombre readOneFromFD (const int fd, char* inputName)
 	struct nombre retour={0,"\0"};
 	uint64_t nombre;
 	ssize_t isReadOK = read(fd,&nombre,sizeof(nombre));
-	nombre = be64toh(nombre);
+	//printf("isReadOK %d\n",isReadOK);
 	if(isReadOK == 0)
 	{
+		printf("isReadOK %lu\n",isReadOK);
 		retour.file="eof";
 	}
 	else if (isReadOK == -1)
@@ -42,9 +48,25 @@ struct nombre readOneFromFD (const int fd, char* inputName)
 		printf("file descriptore : %d\n",fd);
 		retour.file="err";
 	}
-	else
+	else if(isReadOK == sizeof(uint64_t))
 	{
-		//printf("nombre lu : %"PRIu64"\n",nombre);
+		nombre = be64toh(nombre);
+		retour.nombre=nombre;
+		retour.file=inputName;
+	}
+	else{ // cas où la lecture est incomplète
+		size_t toRead=sizeof(nombre)-isReadOK;
+		uint64_t temp=nombre;
+		while(toRead != 0 && isReadOK !=0)
+		{
+			printf("to read : %lu - temp : %"PRIx64"\n",toRead,temp);
+			isReadOK=read(fd,&nombre,toRead);
+			nombre = nombre << toRead*8;
+			temp = temp | nombre;
+			toRead=toRead-isReadOK;
+
+		}
+		nombre = be64toh(temp);
 		retour.nombre=nombre;
 		retour.file=inputName;
 	}
@@ -61,18 +83,20 @@ void* produceFromFD(void* param)
 	while(strcmp(nombre1.file,"eof"))
 	{
 		nombre1 = readOneFromFD(threadParam->fd_read,threadParam->inputName);
-		//printf("read numbre %"PRIu64" from %s\n",nombre1.nombre,nombre1.file);
-		//writeBuffer(threadParam->buffer1,nombre1);
+		printf("read number %"PRIu64" from %s\n",nombre1.nombre,nombre1.file);
+		writeBuffer(threadParam->buffer1,nombre1);
 	}
 	if(threadParam->fd_read!=STDIN_FILENO) //si le descripteur de lecture n'est pas stdin, on le ferme
 	{
+		printf("close fd : %d\n",threadParam->fd_read);
 		close(threadParam->fd_read);
 	}
-	if(threadParam->fd_write!=STDERR_FILENO && threadParam->fd_write != STDOUT_FILENO) // si le descritpeur d'écriture n'est pas stdout ou stderr, on le ferme
+	/*if(threadParam->fd_write!=STDERR_FILENO && threadParam->fd_write != STDOUT_FILENO) // si le descritpeur d'écriture n'est pas stdout ou stderr, on le ferme
 	{
 		close(threadParam->fd_write);
-	}
+	}*/
 	printf("fin de produceFromFD\n");
+	free(param);
 	return NULL;
 }
 
@@ -89,20 +113,18 @@ void* produceFromInternet(void* param) // écrit
 	if(url)
 	{
 		curl_easy_setopt(url,CURLOPT_FAILONERROR,1); // retourne un erreur pour les serveurs http
-		curl_easy_setopt(url,CURLOPT_WRITEDATA,threadParam->fd_write);
 		curl_easy_setopt(url,CURLOPT_WRITEFUNCTION,WriteMemoryPipe);
+		curl_easy_setopt(url,CURLOPT_WRITEDATA,&threadParam->fd_write);
 		//curl_easy_setopt(url,CURLOPT_VERBOSE,1);
 		curl_easy_setopt(url,CURLOPT_URL,threadParam->inputName);
 		error = curl_easy_perform(url);
-		if(error != CURLE_OK)
-		{
-			printf("erreur dans l'ouverture de l'url : %s\n\t%s\n",threadParam->inputName,curl_easy_strerror(error));
-			curl_easy_cleanup(url);
-		}
-		else
-		{
-			curl_easy_cleanup(url);
+		if(error != CURLE_OK) {
+			printf("erreur dans l'ouverture de l'url : %s\n\t%s\n", threadParam->inputName, curl_easy_strerror(error));
 		}
 	}
+	curl_easy_cleanup(url);
+	close(threadParam->fd_write);
+	//produceFromFD(param);
+	free(param);
 	return NULL;
 }
