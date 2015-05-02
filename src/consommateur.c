e01e956c9e20e7da737b44eb43db44a7480b78f5
Damien Deprez <damien.deprez@student.uclouvain.be>
2015-05-02 12:28:39 +0200

    Add consume method without factorisation of the numbre

--- src/consommateur.c
+++ src/consommateur.c
@@ -4,4 +4,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <inttypes.h>
+#include "consommateur.h"
+#include "factorisation.h"
+#include "buffer.h"
+
+void* consumme(void* param)
+{
+	struct consommateur_param* consommateurParam=(struct consommateur_param*)param;
+	struct facteurPremier* local = malloc(sizeof(*local)*SIZE);
+	struct nombre n1;
+	int count=0;
+	if(local)
+	{
+		int isBufferEmpty=0;
+		/*pthread_mutex_lock(consommateurParam->lock);
+		int isProducing=*(consommateurParam->isProducing);
+		pthread_mutex_unlock(consommateurParam->lock);*/
+		int isProducing=1;
+
+		while(isProducing || !isBufferEmpty)
+		{
+			pthread_mutex_lock(consommateurParam->lock);
+			isProducing=*(consommateurParam->isProducing);
+			pthread_mutex_unlock(consommateurParam->lock);
+			isBufferEmpty = readBuffer(consommateurParam->buffer1, &n1);
+			if (isBufferEmpty != 0 || n1.nombre==0) {
+				n1.nombre = 0;
+				n1.file = "end";
+			}
+			else {
+				printf("n1 : %"PRIu64"\n",n1.nombre);
+				count++;
+			}
+		}
+	}
+	printf("count : %d\n",count);
+	free(param);
+	free(local);
+	return NULL;
+}
 
--- src/consommateur.h
+++ src/consommateur.h
@@ -9,4 +9,17 @@
 #ifndef FACTORISATION_CONSOMMATEUR_H
 #define FACTORISATION_CONSOMMATEUR_H
 
+#define SIZE 2048
+
+struct consommateur_param
+{
+	struct buffer* buffer1;
+	pthread_mutex_t* lock;
+	int* isProducing;
+};
+
+void* consumme(void* param);
+
+
+
 #endif //FACTORISATION_CONSOMMATEUR_H
